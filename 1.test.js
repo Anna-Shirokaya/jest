19ce65016edc6aad0c3ccd73102fd10aa93c21d9
Anton <anton.glovatsky@developertools.com>
2024-05-01 22:49:29 +0200

    slenium

--- 1.test.js
+++ 1.test.js
@@ -1,22 +1,5 @@
 const  drawTree  = require('./function/tree.js')
 const fs = require('fs');
-//const { error } = require('console');
-
-afterEach(() => {
-    fs.access('1.txt', fs.constants.F_OK, (err) => {
-        if (!err) {
-          // Если файл существует, удаляем его
-          fs.unlink('1.txt', (err) => {
-            if (err) {
-              //console.error('Ошибка при удалении файла:', err);
-              return;
-            }
-           // console.log('Файл успешно удален');
-          });
-        }
-    });
-    
-});
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -26,16 +9,20 @@ function getRandomInt(min, max) {
 
 const height = getRandomInt(1,1000);
 
+afterEach(() => {
+  const filename = '1.txt'; 
+  if (fs.existsSync(filename)) {
+      fs.unlinkSync(filename);
+  }
+});
+
+
 
 //проверка исключение, передавая строку вместо числа
 test('a string instead of a number', async () => {
     try {
       await drawTree('not a number', '1.txt');
-      // Если код дошел до этой строки, то тест не пройдет, так как не выброшено исключение
-      throw new Error('Function did not throw an error as expected');
     } catch (error) {
-      // Проверяем, что полученное исключение является экземпляром Error
-      expect(error).toBeInstanceOf(Error);
       // Проверяем, что сообщение об ошибке соответствует ожидаемому
       expect(error.message).toBe('Параметр height должен быть числом');
     }
@@ -46,11 +33,7 @@ test('a string instead of a number', async () => {
     // Вызываем функцию с неправильным аргументом
     try {
       await drawTree('', '1.txt');
-      // Если код дошел до этой строки, то тест не пройдет, так как не выброшено исключение
-      throw new Error('Function did not throw an error as expected');
     } catch (error) {
-      // Проверяем, что полученное исключение является экземпляром Error
-      expect(error).toBeInstanceOf(Error);
       // Проверяем, что сообщение об ошибке соответствует ожидаемому
       expect(error.message).toBe('Параметр height должен быть числом');
     }
@@ -61,11 +44,7 @@ test('a string instead of a number', async () => {
     // Вызываем функцию с неправильным аргументом
     try {
       await drawTree(height*(-1), '1.txt');
-      // Если код дошел до этой строки, то тест не пройдет, так как не выброшено исключение
-      throw new Error('Function did not throw an error as expected');
     } catch (error) {
-      // Проверяем, что полученное исключение является экземпляром Error
-      expect(error).toBeInstanceOf(Error);
       // Проверяем, что сообщение об ошибке соответствует ожидаемому
       expect(error.message).toBe('Параметр height должен быть положительным целым числом');
     }
@@ -76,26 +55,18 @@ test('a string instead of a number', async () => {
     // Вызываем функцию с неправильным аргументом
     try {
       await drawTree(0, '1.txt');
-      // Если код дошел до этой строки, то тест не пройдет, так как не выброшено исключение
-      throw new Error('Function did not throw an error as expected');
     } catch (error) {
-      // Проверяем, что полученное исключение является экземпляром Error
-      expect(error).toBeInstanceOf(Error);
       // Проверяем, что сообщение об ошибке соответствует ожидаемому
       expect(error.message).toBe('Параметр height должен быть положительным целым числом');
     }
   });
 
-   //вызываем функцию, передавая 1 вместо числа
+   //вызываем функцию, количество строк =  1 
    test('height = 1', async () => {
     // Вызываем функцию с неправильным аргументом
     try {
       await drawTree(1, '1.txt');
-      // Если код дошел до этой строки, то тест не пройдет, так как не выброшено исключение
-      throw new Error('Function did not throw an error as expected');
     } catch (error) {
-      // Проверяем, что полученное исключение является экземпляром Error
-      expect(error).toBeInstanceOf(Error);
       // Проверяем, что сообщение об ошибке соответствует ожидаемому
       expect(error.message).toBe('Параметр height должен быть положительным целым числом');
     }
@@ -106,11 +77,7 @@ test('a string instead of a number', async () => {
     // Вызываем функцию с неправильным аргументом
     try {
       await drawTree(2.7, '1.txt');
-      // Если код дошел до этой строки, то тест не пройдет, так как не выброшено исключение
-      throw new Error('Function did not throw an error as expected');
     } catch (error) {
-      // Проверяем, что полученное исключение является экземпляром Error
-      expect(error).toBeInstanceOf(Error);
       // Проверяем, что сообщение об ошибке соответствует ожидаемому
       expect(error.message).toBe('Параметр height должен быть положительным целым числом');
     }
@@ -121,11 +88,7 @@ test('a string instead of a number', async () => {
     // Вызываем функцию с неправильным аргументом
     try {
       await drawTree('*', '1.txt');
-      // Если код дошел до этой строки, то тест не пройдет, так как не выброшено исключение
-      throw new Error('Function did not throw an error as expected');
     } catch (error) {
-      // Проверяем, что полученное исключение является экземпляром Error
-      expect(error).toBeInstanceOf(Error);
       // Проверяем, что сообщение об ошибке соответствует ожидаемому
       expect(error.message).toBe('Параметр height должен быть числом');
     }
@@ -135,15 +98,12 @@ test('a string instead of a number', async () => {
   test('Checking the number of rows', async () => { 
     const nameFile = '1.txt';
     await drawTree(height, nameFile);   
-    // Создаем поток для чтения файла
     const readStream = fs.createReadStream(nameFile, { encoding: 'utf8' });
     let lineCount = 0;      
 
     // Обработка события 'data' - данные доступны для чтения
     readStream.on('data', (chunk) => {
-        // Подсчитываем количество строк в очередном куске данных
-        lineCount += chunk.split('\n').length - 1;   
-        //console.log('Количество получилось:', lineCount);        
+        lineCount += chunk.split('\n').length - 1;           
     });
 
     readStream.on('end', () => {
@@ -152,6 +112,11 @@ test('a string instead of a number', async () => {
     
 })
 
-//проверка при недопустимом имени файла
-//Попробуйте записать елочку в файл на максимальной доступной длине пути к файлу.
-//проверка, что количество строк в записываемом файле верное
+//проверка при записи в несуществующее место(несуществующий диск)
+ 
+test('error in the file path', async () => {
+   await drawTree(125, 'E:\\1346.txt');
+   // Проверяем, что файл не был создан
+   expect(fs.existsSync('E:\\1346.txt')).toBe(false);
+});
+
